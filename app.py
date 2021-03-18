8e55cabba9a7cfe501536c7f64e74759148ef0c6
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-03-18 15:34:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/MonitorContingencia/<topic>/<msg>')
+@app.route('/MonitorContingencia/<topic>/<msg>', methods=['GET'])
 def monitor_contingencia(topic, msg):
-    BOT = "1710778365:AAFaexosrl1WMec2al5AQ_D45q00dxHHxHQ"
-    parametros = {'chat_id': int(topic), 'text': str(msg)}
-    requests.post(f'https://api.telegram.org/bot{BOT}/sendMessage', data=parametros)
-    return msg + 'send to ' + topic
+    parametros = dict()
+    try:
+        BOT = "1710778365:AAFaexosrl1WMec2al5AQ_D45q00dxHHxHQ"
+        parametros['chat_id'] = int(topic)
+        parametros['text'] = str(msg)
+
+        requests.post(f'https://api.telegram.org/bot{BOT}/sendMessage', data=parametros)
+    except:
+        parametros['error'] = 'No se pudo enviar el mensaje a telegram'
+    return jsonify(parametros)
 
 
 if __name__ == '__main__':
