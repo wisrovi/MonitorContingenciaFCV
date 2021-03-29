3eb124b906c055fcee14225d3e800739f84f231d
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-03-29 15:17:49 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,13 +1,23 @@
 
 import os.path as path
+from decouple import config
 
 from Util.autoupdate import Autoupdate
 
 
-@Autoupdate(name="Autoupdate WISROVI", project="wisrovi/MANITOR-Raspberry", root_path=path.dirname(path.realpath(__file__)))
+@Autoupdate(name="Autoupdate WISROVI", project=config('project', default=''), root_path=path.dirname(path.realpath(__file__)))
 def main_demo_autoupdate():
     print("update library")
 
 
 if __name__ == "__main__":
+
+    print()
+    print("*********************************************************")
+    print("*\t", "Autor: " "\t\t\t\t\t\t\t\t\t\t\t*")
+    print("*\t", config('autor', default=''), "\t\t\t\t*")
+    print("*\t", config('alias_autor', default=''), "\t\t\t\t\t\t\t\t\t\t\t*")
+    print("*\t", config('email_autor', default=''), "\t\t\t\t\t\t*")
+    print("*\t", config('linkedin_autor', default=''), "\t*")
+    print("*********************************************************")
     main_demo_autoupdate()
