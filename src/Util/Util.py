b9c3a5ccc75d4a13d1c51f26827b1ba3dd4a2837
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-06-15 13:43:19 -0500

    Update Util.py

--- src/Util/Util.py
+++ src/Util/Util.py
@@ -3,3 +3,6 @@ class Util(object):
 
     def decoBase64UrlSafe(self, s):
         return self.base64.urlsafe_b64decode(s + '=' * (4 - len(s) % 4))
+
+    def decodeBase64(self, s):
+        return self.base64.b64decode(s)
